4d6ca33031447b83544c1482ed2c7883363250b5
Julius Simonelli <julius.simonelli@gmail.com>
2015-02-15 22:44:44 -0500

    Working up to 532
    About to add power ups - divergence in unity versions!

--- Space SHMUP/Assets/Scripts/Enemy.cs
+++ Space SHMUP/Assets/Scripts/Enemy.cs
@@ -7,22 +7,38 @@ public class Enemy : MonoBehaviour {
 	public float		fireRate = 0.3f; // Seconds per shot (Unused)
 	public float		health = 10;
 	public int			score = 100; //Points earned for destroying this
-	
+
+	public int			showDamageForFrames = 2; // # of frames to show damage
+
 	public bool _________________;
+
+	public Color[]		originalColors;
+	public Material[]	materials; //All the Materials of this & its children
+	public int			remainingDamageFrames = 0; // Damage frames left
 
 	public Bounds bounds; //The Bounds of this and its children
 	public Vector3 boundsCenterOffset; //Distance of bounds.center from position
 	
 	void Awake() {
-
+		materials = Utils.GetAllMaterials (gameObject);
+		originalColors = new Color[materials.Length];
+		for (int i = 0; i < materials.Length; i++) {
+			originalColors [i] = materials [i].color;
+		}
 		InvokeRepeating ("CheckOffscreen", 0f, 2f);
 	}
 	
 	//Update is called once per frame
 	void Update(){
 		Move();
-
+		if (remainingDamageFrames > 0) {
+			remainingDamageFrames--;
+			if (remainingDamageFrames == 0) {
+				UnShowDamage ();
+			}
+		}
 	}
+
 	
 	public virtual void Move(){
 		Vector3 tempPos = pos;
@@ -59,6 +75,45 @@ public class Enemy : MonoBehaviour {
 				//then destroy it
 				Destroy (this.gameObject);
 			}
+		}
+	}
+
+	void OnCollisionEnter (Collision coll) {
+		GameObject other = coll.gameObject;
+		switch (other.tag) {
+		case "ProjectileHero":
+			Projectile p = other.GetComponent<Projectile> ();
+			// Enemies don't take damage unless they're onscreen
+			// This stops the player from shooting them before they are visible
+			bounds.center = transform.position + boundsCenterOffset;
+			if (bounds.extents == Vector3.zero || Utils.ScreenBoundsCheck (bounds, BoundsTest.offScreen) != Vector3.zero) {
+				Destroy (other);
+				break;
+			}
+			// Hurt this Enemy
+			ShowDamage();
+			// Get the damage amount from the Projectile.type & Main.W_DEFS
+			health -= Main.W_DEFS [p.type].damageOnHit;
+			if (health <= 0) {
+				// Tell the Main singleton that this ship has been destroyed
+		//		Main.S.ShipDestroyed(this);
+				// Destroy this Enemy
+				Destroy (this.gameObject);
+			}
+			Destroy (other);
+			break;
+		}
+	}
+
+	void ShowDamage() {
+		foreach (Material m in materials) {
+			m.color = Color.red;
+		}
+		remainingDamageFrames = showDamageForFrames;
+	}
+	void UnShowDamage() {
+		for (int i = 0; i < materials.Length; i++) {
+			materials[i].color = originalColors[i];
 		}
 	}
 
