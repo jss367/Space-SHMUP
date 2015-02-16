f40e63f69cc5ca33429b3ca696d6419ec6de443c
Julius Simonelli <julius.simonelli@gmail.com>
2015-02-15 23:43:12 -0500

    Page 541 Got here with working version
    Believe I found a } error in OnTriggerEnter of Hero.cs. Also, found that
    TriggerGo was misspelled, but it didn't affect game play

--- Space SHMUP/Assets/Scripts/Hero.cs
+++ Space SHMUP/Assets/Scripts/Hero.cs
@@ -17,6 +17,9 @@ public class Hero : MonoBehaviour {
 	[SerializeField]
 	private float _shieldLevel = 1;
 
+	// Weapon fields
+	public Weapon[]			weapons;
+
 	public bool _____________;
 
 	public Bounds				bounds;
@@ -29,6 +32,12 @@ public class Hero : MonoBehaviour {
 	void Awake(){
 		S = this; //Set the singleton
 		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+	}
+
+	void Start() {
+		// Reset the weapons to start _Hero with 1 blaster
+		ClearWeapons ();
+		weapons [0].SetType (WeaponType.blaster);
 	}
 
 	
@@ -85,7 +94,9 @@ public class Hero : MonoBehaviour {
 				shieldLevel--;
 				//Destroy the enemy
 				Destroy(go);
-
+			}else if (go.tag == "PowerUp") {
+				// If the sheild was triggered by a PowerUp
+				AbsorbPowerUp(go);
 			}else{
 			//Announce it
 			print ("Triggered: " + go.name);
@@ -106,9 +117,51 @@ public class Hero : MonoBehaviour {
 			if (value < 0) {
 				Destroy (this.gameObject);
 				//Tell Main.S to restart the game after a delay
-				Main.S.DelayedRestart(gameRestartDelay);
+				Main.S.DelayedRestart (gameRestartDelay);
 			}
 		}
 	}
+		public void AbsorbPowerUp(GameObject go) {
+			PowerUp pu = go.GetComponent<PowerUp>();
+			switch (pu.type) {
+			case WeaponType.shield: // If it's the shield
+				shieldLevel++;
+				break;
+				
+			default: // If it's any Weapon PowerUp
+				// Check the current weapon type
+				if (pu.type == weapons[0].type) {
+					// then increase the number of weapons of this type
+					Weapon w = GetEmptyWeaponSlot(); // Find an available weapon
+					if (w != null) {
+						// Set it to pu.type
+						w.SetType(pu.type);
+					}
+				} else {
+					// If this is a different weapon
+					ClearWeapons();
+					weapons[0].SetType(pu.type);
+				}
+				break;
+			}
+			pu.AbsorbedBy(this.gameObject);
+		}
+		
+		Weapon GetEmptyWeaponSlot() {
+			for (int i = 0; i < weapons.Length; i++) {
+				if (weapons[i].type == WeaponType.none) {
+					return(weapons[i]);
+				}
+			}
+			return(null);
+		}
+		
+		void ClearWeapons() {
+			foreach (Weapon w in weapons) {
+				w.SetType(WeaponType.none);
+			}
+		}
+
+	
 
 }
--- Space SHMUP/Assets/Scripts/Weapon.cs
+++ Space SHMUP/Assets/Scripts/Weapon.cs
@@ -50,8 +50,12 @@ public class Weapon : MonoBehaviour {
 	public GameObject collar;
 	public float lastShot; //Time last shot was fired
 
-	void Start() {
+	void Awake() {
 		collar = transform.Find ("Collar").gameObject;
+
+	}
+
+	void Start() {
 		//Call SetType() properly for the default _type
 		SetType (_type);
 		
