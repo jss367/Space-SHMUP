b704f49bf4cc135096ca2bbe5abb4a6c83586b69
Julius Simonelli <julius.simonelli@gmail.com>
2015-02-28 13:31:40 -0500

    added 5 min gui

---
+++ Space SHMUP/Assets/ClickSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Prime31;
+namespace FMG
+{
+	public class ClickSelect : MonoBehaviourGUI, IPointerClickHandler {
+		
+		public virtual void OnPointerClick(PointerEventData eventData)
+		{
+			Debug.Log ("ClickEvent" );
+			MediaPlayerBinding.showMediaPicker();
+		}
+	}
+}
