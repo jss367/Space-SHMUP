6eb44d51d35839d822f50d512cc951a787d86c72
Julius Simonelli <julius.simonelli@gmail.com>
2015-04-29 21:29:50 -0400

    removed some things

--- Space SHMUP/Assets/Scripts/Explosion.cs
+++ Space SHMUP/Assets/Scripts/Explosion.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SU_Explosion : MonoBehaviour {
+public class Explosion : MonoBehaviour {
 	public float destroyAfterSeconds = 8.0f;
-
+	
 	void Awake() {
 		// Destroy gameobject after delay
 		Destroy(gameObject, destroyAfterSeconds);
