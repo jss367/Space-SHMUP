e7062a8e3ac2e011c3af3c458f7a52c9b9b8fb07
Julius Simonelli <julius.simonelli@gmail.com>
2015-04-30 21:24:14 -0400

    added enemy fire

--- Space SHMUP/Assets/SpaceUnity/Scripts/SU_Asteroid.cs
+++ Space SHMUP/Assets/SpaceUnity/Scripts/SU_Asteroid.cs
@@ -138,12 +138,12 @@ public class SU_Asteroid : MonoBehaviour {
 				Instantiate(explosion, transform.position, transform.rotation);
 			}else{
 				//Announce it
-				print ("Triggered: " + go.name);
+//				print ("Triggered: " + go.name);
 				//Make sure it's not the same triggering go as last time
 			}
 		}else {
 			//Otherwise announce the original gameObject
-			print ("Triggered: " + other.gameObject.name);
+//			print ("Triggered: " + other.gameObject.name);
 		}
 	}
 
