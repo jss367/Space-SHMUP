249e1dd608a8e067ec195df6b0d912399be9c753
Julius Simonelli <julius.simonelli@gmail.com>
2015-05-16 19:40:25 -0400

    optimizing first level

--- Space SHMUP/Assets/Level1Manager.cs
+++ Space SHMUP/Assets/Level1Manager.cs
@@ -3,13 +3,52 @@ using System.Collections;
 
 public class Level1Manager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GameObject AsteroidSpawn1;
+	public GameObject AsteroidSpawn2;
+	public GameObject AsteroidSpawn3;
+
+	public GameObject EnemySpawn1;
+	public GameObject EnemySpawn2;
+	public GameObject EnemySpawn3;
+
+	public float timeLimit;
+
+	public float firstBreak;
+	public float secondBreak;
+	public float thirdBreak;
+
+	/*
+variables that should be changed by level:
+spawn breaks
+victorypoints
+
+*/
+	//Private reference for this class only
+//	private static Level1Manager _instance;
+
+	//Public reference for other classes
+	public static Level1Manager instance { get; private set;}
+
+	void Awake()
+	{
+		instance = this;
+		timeLimit = GameObject.Find ("Beat").GetComponent<AudioManager> ().timeLimit;
+//		firstBreak = timeLimit / 4;
+//		secondBreak = timeLimit / 2;
+//		thirdBreak = 3 * timeLimit / 4;
+
+		firstBreak = 4;
+		secondBreak = 10;
+		thirdBreak = 13;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+//	public string GetValues()
+//	{
+//	}
+//
+//	public float GetFloats()
+//	{
+//
+//	}
+
 }
--- Space SHMUP/Assets/_Scripts/SpawnManager.cs
+++ Space SHMUP/Assets/_Scripts/SpawnManager.cs
@@ -9,9 +9,13 @@ public class SpawnManager : MonoBehaviour {
 	public float firstBreak;
 	public float secondBreak;
 	public float thirdBreak;
-	public float timeLimit;
+//	public float timeLimit;
+
+	public GameObject spawn1;
+
 
 	public GameObject AsteroidSpawn1;
+//	public GameObject AsteroidSpawn11;
 	public GameObject AsteroidSpawn2;
 	public GameObject AsteroidSpawn3;
 
@@ -25,10 +29,22 @@ public class SpawnManager : MonoBehaviour {
 		if (mainObject != null) {
 			main = mainObject.GetComponent<Main> ();
 		}
-		timeLimit = GameObject.Find ("Beat").GetComponent<AudioManager> ().timeLimit;
-		firstBreak = timeLimit / 4;
-		secondBreak = timeLimit / 2;
-		thirdBreak = 3 * timeLimit / 4;
+//		timeLimit = GameObject.Find ("Beat").GetComponent<AudioManager> ().timeLimit;
+//		firstBreak = timeLimit / 4;
+//		secondBreak = timeLimit / 2;
+//		thirdBreak = 3 * timeLimit / 4;
+
+		firstBreak = Level1Manager.instance.firstBreak;
+		secondBreak = Level1Manager.instance.secondBreak;
+		thirdBreak = Level1Manager.instance.thirdBreak;
+
+		AsteroidSpawn1 = Level1Manager.instance.AsteroidSpawn1;
+		AsteroidSpawn2 = Level1Manager.instance.AsteroidSpawn2;
+		AsteroidSpawn3 = Level1Manager.instance.AsteroidSpawn3;
+
+		EnemySpawn1 = Level1Manager.instance.EnemySpawn1;
+		EnemySpawn2 = Level1Manager.instance.EnemySpawn2;
+		EnemySpawn3 = Level1Manager.instance.EnemySpawn3;
 	}
 	
 	// Update is called once per frame
@@ -38,6 +54,9 @@ public class SpawnManager : MonoBehaviour {
 			level = 1;
 		//	Debug.Log("The time alive is " + main.timeAlive);
 		//	Debug.Log("The first break is " + firstBreak);
+			AsteroidSpawn1.SetActive(true);
+			EnemySpawn1.SetActive(true);
+
 		} else if (timer >= firstBreak && timer < secondBreak) {
 			level = 2;
 			AsteroidSpawn1.SetActive(false);
