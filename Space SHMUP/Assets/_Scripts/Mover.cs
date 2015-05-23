11e5643c8bc2370eedd0ab33498f4fa4caa7f372
Julius Simonelli <julius.simonelli@gmail.com>
2015-05-23 17:38:44 -0400

    Changed music, fixed enemy shot

--- Space SHMUP/Assets/_Scripts/Mover.cs
+++ Space SHMUP/Assets/_Scripts/Mover.cs
@@ -5,9 +5,20 @@ public class Mover : MonoBehaviour {
 
 
 		public float speed;
+
+	void Awake(){
+		//Test to see whether this has passed off screen every 2 seconds
+		InvokeRepeating ("CheckOffscreen", 2f, 2f);
+	}
 		
 		void Start ()
 		{
 			GetComponent<Rigidbody>().velocity = transform.forward * speed;
 		}
+
+	void CheckOffscreen() {
+		if (Utils.ScreenBoundsCheck (GetComponent<Collider>().bounds, BoundsTest.offScreen) != Vector3.zero) {
+			Destroy (this.gameObject);
+		}
+	}
 	}
